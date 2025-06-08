3a8472a1068d7385a29542cad684757c71b29009
HarkiIsono <haruki.isono861@gmail.com>
2025-06-08 17:06:13 +0900

    fix

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-package_name = 'human_detection_yolov8'
+package_name = 'human_detection'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.1.0',
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='haaruki',
-    maintainer_email='haruki.isono861@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='MoveNet-based pose estimation node for ROS 2',
+    license='Apache-2.0',
     entry_points={
         'console_scripts': [
-            'human_detection_node = human_detection_yolov8.human_detection_node:main'
+            'movenet_node = human_detection.movenet_node:main',
         ],
     },
 )
